fd1d7dd74307a54a4bf7b7443849510517c3fc03
sam e thavy <samy2000ws@gmail.com>
2024-09-03 17:44:32 -0300

    atividades 1 e 2

--- exemplo3.py
+++ exemplo3.py
@@ -6,4 +6,4 @@ nota3 = (input("Informe a 3ª nota:"))
 media = (nota1+nota2+nota3)/3
 
 print(f"Sua média é {media:.2f}")#.2f significa que teremos 2 casas decimais após a vírgula 
-#Trabal
+#Trabalhando 
