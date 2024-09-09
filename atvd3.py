e09ec4ba5ebde2de54d4c231e01e49a401232d2e
samu e thavy <samy2000ws@gmail.com>
2024-09-09 14:38:51 -0300

    Trabalhando com estrutura para no python

---
+++ atvd3.py
@@ -0,0 +1,11 @@
+a = float(input("Informe o valor de a: "))
+b = float(input("Digite o valor de b:  "))
+c = float(input("Digite o valor de c:   ")) 
+
+soma = a+b
+
+if soma < c :
+
+     print ("a soma d a e b e menor que c") 
+else :
+ print("a soma d a e b e menor que c")
