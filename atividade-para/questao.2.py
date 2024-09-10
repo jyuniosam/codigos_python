63f72aaf7351463a3a92bd2a3b927792336890c4
SilmesonS <silmesonsilmeson@gmail.com>
2024-09-10 16:28:59 -0300

    questao 1

--- atividade-para/questao.2.py
+++ atividade-para/questao.2.py
@@ -3,10 +3,10 @@ contador_maior_igual_18 = 0
 
 # Solicitar a idade de 5 pessoas 
 for idade in range(1,6):
-idadee = int(input(f"Digital a idade da pessoa {idade}: "))
+idade = int(input(f"Digital a idade da pessoa {idade}: "))
 # Verificar se a idade é maior ou igual a 18 
 if idade >= 18:
     contador_maior_igual_18 += 1 
 
     #Mostrar o resultado  
-    print(f"Quantidade de pessoas com 18 anos ou mais: {contador_maior_igual_18}")
+    print(f"Quantidade de pessoas com 18 anos ou mais: {contador_maior_igual_18}") 
